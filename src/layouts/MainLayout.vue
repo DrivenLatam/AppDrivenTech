2f3b4e652221202733f1a5ba007deb726f765f9b
jesus <jesusromanm99@fpuna.edu.py>
2021-10-13 15:36:53 -0300

    Se agrega capacitor

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-
     <q-footer bordered class="bg-transparent">
       <q-toolbar class="bg-transparent flex flex-center">
         <q-tabs v-model="tab" dense class="text-grey-6 full-width" align="justify" 
