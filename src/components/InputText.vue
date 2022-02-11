a740acd2722837339f14cfe8958c6cb0cfd75404
jesus <jesusromanm99@fpuna.edu.py>
2022-02-11 11:32:12 -0300

    Se cambia estilo del componente InputText y los colores primarios y secundarios

---
+++ src/components/InputText.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="input-container row justify-between items-center">
+    <div class="column justify-evenly">
+      <p class="input-container__label text-weight-light">{{ label }}</p>
+      <p class="input-container__text text-weight-regular">{{ value }}</p>
+    </div>
+    <q-icon  @click="onWhatsAppClick" v-if="whatsapp" 
+            color="green-8" name="whatsapp" size="1.8rem"
+            class="whatsapp-icon"
+             />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, toRefs } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "InputText",
+  props: ["label", "value","whatsapp"],
+  setup(props, { emit }) {
+    const { label, value,whatsapp } = toRefs(props);
+    const onWhatsAppClick=()=>{
+        emit("onWhatsAppClick")
+    }
+    return {onWhatsAppClick};
+  },
+});
+</script>
+<style lang="scss" scoped>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+.input-container {
+ 
+  margin: 5px 0px;
+  background-color: rgba(33, 33, 33, 0.025);
+  border-bottom: 1px solid  $grey-6;
+  border-radius: 5px 5px 0px 0px;
+}
+.input-container > div{
+  height: 3.5rem;
+}
+.input-container__label {
+  color: #000;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 0.9rem;
+  
+}
+
+.input-container__text {
+  font-size: 1rem;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+ 
+  color: rgba(0, 0, 0, 0.8);
+}
+
+.input-container p {
+  margin-left: 8px;
+}
+.whatsapp-icon{
+  margin-right: 0.5rem;
+}
+</style>
